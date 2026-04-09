4f7306168b8be30653d1837b70372c9994fedf15
tonstrina <paddycarr@gmail.com>
2026-04-09 15:16:17 +0100

    Replace Vercel Postgres with Neon database

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,5 @@
-import { sql } from '@vercel/postgres'
+import { neon } from '@neondatabase/serverless'
+const sql = neon(process.env.POSTGRES_URL!)
 
 export async function initDB() {
   await sql`
